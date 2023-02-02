196ecd5a4a77c8813022972e4f2e6a1b7030f775
aneldjesevic <117862081+aneldjesevic@users.noreply.github.com>
2023-02-02 22:05:49 +0100

    js update

--- main.js
+++ main.js
@@ -35,3 +35,32 @@ var isOpen = Boolean = true;
             }
         });
         
+        function openTab(evt, tabName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(tabName).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+        document.getElementById("loginTab").click();
+
+        var password = document.getElementById("etSPassword")
+            , confirm_password = document.getElementById("etConfirmPassword");
+
+        function validatePassword() {
+            if (confirm_password.value != password.value) {
+                confirm_password.setCustomValidity("Passwords Don't Match");
+            } else {
+                confirm_password.setCustomValidity('');
+            }
+        }
+
+        password.onchange = validatePassword;
+        confirm_password.onkeyup = validatePassword;
+
